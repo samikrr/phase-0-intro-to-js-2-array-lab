ba89f5e6726e5a98b3a0536193d3fa5e45088167
Samuel Kirori <samuelkirori300@gmail.com>
2024-03-23 19:18:58 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependcat(name){
+    return cats.unshift(...cats,name);
+}
+
+function destructivelyRemoveLastCat(name){
+return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+   return cats.shift(name);
+}
+
+function AppendCat(name){
+   return [...cats,name];
+}
+appendCat("Broom");
+
+    function prependCat(name) {
+     return [name,...cats] ;  
+}
+prependCat("Arnold");
+
+function removeLastCat(name){
+    return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
